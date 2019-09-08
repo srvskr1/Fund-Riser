68435326e551accd496ee25c7280acedd7eb430f
srvskr1 <52565422+srvskr1@users.noreply.github.com>
2019-09-08 21:49:54 +0600

    v-1

---
+++ fund riser/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Fundrise.com</title>
+    
+  </head>
+  <body>
+    
+ <a href="/login">Login</a>
+  <a href="{{route('customer.registration')}}">Create Your Account</a>
+    
+  </body>
+</html>
